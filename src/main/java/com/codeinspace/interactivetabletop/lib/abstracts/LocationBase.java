8fb0fd0503a87c8dca15487624bd864878586a45
Brian Blankenship <c0dezer019@zohomail.com>
2022-09-15 01:55:54 -0500

    Renaming values and javafx for space simulation.

--- src/main/java/com/codeinspace/interactivetabletop/lib/abstracts/LocationBase.java
+++ src/main/java/com/codeinspace/interactivetabletop/lib/abstracts/LocationBase.java
@@ -1,4 +1,28 @@
 package com.codeinspace.interactivetabletop.lib.abstracts;
 
-public class LocationBase {
+import javafx.geometry.Point3D;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public abstract class LocationBase {
+    private final UUID UID;
+    private String name;
+    private String desc;
+    private Map<String, Point3D> coords = new HashMap<>();
+
+    public LocationBase(String name, String desc, Point3D @NotNull [] coords) {
+        this.name = name;
+        this.desc = desc;
+        this.coords.put("x", coords[0]);
+        this.coords.put("y", coords[1]);
+        this.coords.put("z", coords[2]);
+        this.UID = UUID.randomUUID();
+    }
+
+    protected String getName() { return name; }
+    protected void setName(String name) { this.name = name; }
+
 }
